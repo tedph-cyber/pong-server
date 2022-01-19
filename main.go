ec5f78f72bea4975c433dc0b7ef543c7cf3e039e
Jubril Oyetunji <cyberdev01@protonmail.com>
2022-01-19 20:41:52 +0100

    make port a flag

--- main.go
+++ main.go
@@ -1,27 +1,26 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"os"
 )
 
 var(
-	args = os.Args
+	port = flag.String("port",":8080","port the server would run on")
 )
 func main(){
+  flag.Parse()
   l := log.New(os.Stdout,"[PONG] ",log.LstdFlags)
-  port := args[1]
-  if port == ""{
-	  port = ":8080"
-  }
   r := http.NewServeMux()
+
   r.HandleFunc("/ping",func(rw http.ResponseWriter, r *http.Request) {
 	  l.Printf("request recieved from %s using %s",r.RemoteAddr,r.Method)
 	  rw.Write([]byte("pong"))
   })
-  l.Printf("booted up pong server on %s",port)
-  err := http.ListenAndServe(port,r)
+  l.Printf("booted up pong server on %s",*port)
+  err := http.ListenAndServe(*port,r)
   if err != nil {
 	  l.Fatal(err)
   }
